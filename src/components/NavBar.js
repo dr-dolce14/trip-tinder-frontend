a6f8d80fb9d42b426067777d682cfd282acec019
mayalexandera <maya.blevins@gmail.com>
2020-08-14 11:27:26 -0400

    buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,24 @@ import '../resources/css/MainNav.css'
 function NavBar() {
     return (
       <header>
+        <nav>
+          <div className='row'>
+            <div className='main-nav js--main-nav'>
+              <NavLink to='/trips'>Trips</NavLink>
+              <NavLink to='/parks'>Parks</NavLink>
+              <NavLink to='/about'>About Trinder</NavLink>
+              <NavLink to='/signup'>Sign up</NavLink>
+              <NavLink to='/login'>Log In</NavLink>
+            </div>
+          </div>
+        </nav>
+
         <div className='row'>
-          <div className='main-nav js--main-nav'>
-            <NavLink to='/trips'>Trips</NavLink>
-            <NavLink to='/parks'>Parks</NavLink>
-            <NavLink to='/about'>About Trinder</NavLink>
-            <NavLink to='/signup'>Sign up</NavLink>
-            <NavLink to='/login'>Log In</NavLink>
+          <div className='hero-text-box'>
+            <h1>
+              The clearest way into the Universe is through a forest wilderness.
+            </h1>{" "}
+            <br /> <h1>How will you get there?</h1>
           </div>
         </div>
       </header>
