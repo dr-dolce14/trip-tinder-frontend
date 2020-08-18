50c5d3bbea48cd1c916981e3085679821eaecb7f
mayalexandera <maya.blevins@gmail.com>
2020-08-18 19:53:50 -0400

    styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,11 @@ class Welcome extends React.Component {
   render() {
     return (
       <section className='landing' id='about-trinder'>
-        <div>
+        <div className='row'>
           <h2>Who We Are</h2>
           <p className="long-copy">
-            At Trinder- we know the value of the outdoors, and the power of connecting to nature.  Our mission is to inspire people to explore  while bringing  closer together.
+            Here at Trinder, we know the importance of the preservation and the power of connecting to nature.  Our mission is to inspire people to explore while bringing them closer together.
           </p>
-
-
           {/* <h2>“Between every two pines is a doorway to a new world.”</h2>
           <h2>“One touch of nature makes the whole world kin.”</h2> */}
         </div>
